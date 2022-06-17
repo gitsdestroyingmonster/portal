c3f6c032695d90bf093e9904fe3621b1433d9f39
mjairumi <mohammadjairumi43@gmail.com>
2022-06-17 11:34:20 +0530

    Added Structure

---
+++ src/components/Head.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Head (){
+    return (
+        <header>Hello</header>
+    )
+}
+
+export default Head;
---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/sidebar.css'
+
+function SideBar(){
+    return (
+        <div className='side-bar'>
+            SideBar
+        </div>
+    )
+}
+
+export default SideBar;
